5234a78d688d709972366daf0a9e9efa502d3130
Qiang Ma <mq7253180@126.com>
2020-01-16 10:17:54 +0800

    xxx

--- sftp/src/main/java/com/quincy/core/sftp/PoolableChannelSftp.java
+++ sftp/src/main/java/com/quincy/core/sftp/PoolableChannelSftp.java
@@ -1,14 +1,9 @@
 package com.quincy.core.sftp;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Vector;
-
 import org.apache.commons.pool2.ObjectPool;
 
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.SftpException;
 
 public class PoolableChannelSftp extends ChannelSftp {
 	private ChannelSftp channelSftp;
@@ -30,29 +25,5 @@ public class PoolableChannelSftp extends ChannelSftp {
 	public void reallyDisconnect() throws JSchException {
 		this.channelSftp.disconnect();
 		this.channelSftp.getSession().disconnect();
-	}
-
-	public void cd(String path) throws SftpException{
-		this.channelSftp.cd(path);
-	}
-
-	public boolean isConnected() {
-		return this.channelSftp.isConnected();
-	}
-
-	public boolean isClosed() {
-		return this.channelSftp.isClosed();
-	}
-
-	public Vector ls(String path) throws SftpException{
-		return this.channelSftp.ls(path);
-	}
-
-	public void put(InputStream src, String dst) throws SftpException{
-		this.channelSftp.put(src, dst);
-	}
-
-	public void get(String src, OutputStream dst) throws SftpException{
-		this.channelSftp.get(src, dst);
 	}
 }
