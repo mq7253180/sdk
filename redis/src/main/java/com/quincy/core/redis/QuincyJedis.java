0d9b9e808e9a0abb5074273646b20e8d0a77d0b0
Qiang Ma <mq7253180@126.com>
2020-02-14 11:26:13 +0800

    xxx

--- redis/src/main/java/com/quincy/core/redis/QuincyJedis.java
+++ redis/src/main/java/com/quincy/core/redis/QuincyJedis.java
@@ -2,15 +2,24 @@ package com.quincy.core.redis;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.BitOP;
 import redis.clients.jedis.BitPosParams;
+import redis.clients.jedis.Client;
 import redis.clients.jedis.ClusterReset;
 import redis.clients.jedis.DebugParams;
+import redis.clients.jedis.GeoCoordinate;
+import redis.clients.jedis.GeoRadiusResponse;
+import redis.clients.jedis.GeoUnit;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.ScanParams;
+import redis.clients.jedis.ScanResult;
 import redis.clients.jedis.JedisCluster.Reset;
+import redis.clients.jedis.params.geo.GeoRadiusParam;
 
 @Slf4j
 public class QuincyJedis extends Jedis {
@@ -460,49 +469,406 @@ public class QuincyJedis extends Jedis {
 	}
 
 	@Override
-	  public String echo(final String string) {
+	public String echo(final String string) {
 		return jedisCluster.echo(string);
 	}
 
 	@Override
-	  public byte[] echo(final byte[] string) {
+	public byte[] echo(final byte[] string) {
 		return jedisCluster.echo(string);
 	}
 
 	@Override
-	  public Object eval(final String script) {
+	public Object eval(final String script) {
 		throw new RuntimeException(EXCEPTION_MSG);
 	}
 
 	@Override
-	  public Object eval(final byte[] script) {
+	public Object eval(final byte[] script) {
 		throw new RuntimeException(EXCEPTION_MSG);
 	}
 
 	@Override
-	  public Object eval(final byte[] script, final byte[] keyCount, final byte[]... params) {
+	public Object eval(final byte[] script, final byte[] keyCount, final byte[]... params) {
 		return jedisCluster.eval(script, keyCount, params);
 	}
 
 	@Override
-	  public Object eval(final byte[] script, final int keyCount, final byte[]... params) {
+	public Object eval(final byte[] script, final int keyCount, final byte[]... params) {
 		return jedisCluster.eval(script, keyCount, params);
 	}
 
 	@Override
-	  public Object eval(final byte[] script, final List<byte[]> keys, final List<byte[]> args) {
+	public Object eval(final byte[] script, final List<byte[]> keys, final List<byte[]> args) {
 		return jedisCluster.eval(script, keys, args);
 	}
 
 	@Override
-	  public Object eval(final String script, final int keyCount, final String... params) {
+	public Object eval(final String script, final int keyCount, final String... params) {
 		return jedisCluster.eval(script, keyCount, params);
 	}
 
 	@Override
-	  public Object eval(final String script, final List<String> keys, final List<String> args) {
+	public Object eval(final String script, final List<String> keys, final List<String> args) {
+		return jedisCluster.eval(script, keys, args);
+	}
+
+	@Override
+	public String flushAll() {
+		return jedisCluster.flushAll();
+	}
+
+	@Override
+	public String flushDB() {
+		return jedisCluster.flushDB();
+	}
+
+	@Override
+	public Long geoadd(final String key, final Map<String, GeoCoordinate> memberCoordinateMap) {
+		return jedisCluster.geoadd(key, memberCoordinateMap);
+	}
+
+	@Override
+	public Long geoadd(final byte[] key, final Map<byte[], GeoCoordinate> memberCoordinateMap) {
+		return jedisCluster.geoadd(key, memberCoordinateMap);
+	}
+
+	@Override
+	public Long geoadd(final String key, final double longitude, final double latitude, final String member) {
+		return jedisCluster.geoadd(key, longitude, latitude, member);
+	}
+
+	@Override
+	public Long geoadd(final byte[] key, final double longitude, final double latitude, final byte[] member) {
+		return jedisCluster.geoadd(key, longitude, latitude, member);
+	}
+
+	@Override
+	public Double geodist(final String key, final String member1, final String member2) {
+		return jedisCluster.geodist(key, member1, member2);
+	}
+
+	@Override
+	public Double geodist(final byte[] key, final byte[] member1, final byte[] member2) {
+		return jedisCluster.geodist(key, member1, member2);
+	}
+
+	@Override
+	public Double geodist(final String key, final String member1, final String member2, final GeoUnit unit) {
+		return jedisCluster.geodist(key, member1, member2, unit);
+	}
+
+	@Override
+	public Double geodist(final byte[] key, final byte[] member1, final byte[] member2, final GeoUnit unit) {
+		return jedisCluster.geodist(key, member1, member2, unit);
+	}
+
+	@Override
+	public List<String> geohash(final String key, String... members) {
+		return jedisCluster.geohash(key, members);
+	}
+
+	@Override
+	public List<byte[]> geohash(final byte[] key, byte[]... members) {
+		return jedisCluster.geohash(key, members);
+	}
+
+	@Override
+	public List<GeoCoordinate> geopos(final String key, String... members) {
+		return jedisCluster.geopos(key, members);
+	}
+
+	@Override
+	public List<GeoCoordinate> geopos(final byte[] key, byte[]... members) {
+		return jedisCluster.geopos(key, members);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadius(final String key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit) {
+		return jedisCluster.georadius(key, longitude, latitude, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadius(final byte[] key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit) {
+		return jedisCluster.georadius(key, longitude, latitude, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadius(final String key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadius(key, longitude, latitude, radius, unit, param);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadius(final byte[] key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadius(key, longitude, latitude, radius, unit, param);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMember(final String key, final String member, final double radius,
+			final GeoUnit unit) {
+		return jedisCluster.georadiusByMember(key, member, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMember(final byte[] key, final byte[] member, final double radius,
+			final GeoUnit unit) {
+		return jedisCluster.georadiusByMember(key, member, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMember(final String key, final String member, final double radius,
+			final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadiusByMember(key, member, radius, unit, param);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMember(final byte[] key, final byte[] member, final double radius,
+			final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadiusByMember(key, member, radius, unit, param);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMemberReadonly(final String key, final String member, final double radius,
+			final GeoUnit unit) {
+		return jedisCluster.georadiusByMemberReadonly(key, member, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMemberReadonly(final byte[] key, final byte[] member, final double radius,
+			final GeoUnit unit) {
+		return jedisCluster.georadiusByMemberReadonly(key, member, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMemberReadonly(final String key, final String member, final double radius,
+			final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadiusByMemberReadonly(key, member, radius, unit, param);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusByMemberReadonly(final byte[] key, final byte[] member, final double radius,
+			final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadiusByMemberReadonly(key, member, radius, unit, param);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusReadonly(final String key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit) {
+		return jedisCluster.georadiusReadonly(key, longitude, latitude, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusReadonly(final byte[] key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit) {
+		return jedisCluster.georadiusReadonly(key, longitude, latitude, radius, unit);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusReadonly(final String key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadiusReadonly(key, longitude, latitude, radius, unit, param);
+	}
+
+	@Override
+	public List<GeoRadiusResponse> georadiusReadonly(final byte[] key, final double longitude, final double latitude,
+			final double radius, final GeoUnit unit, final GeoRadiusParam param) {
+		return jedisCluster.georadiusReadonly(key, longitude, latitude, radius, unit, param);
+	}
+
+	@Override
+	public String get(final String key) {
+		return jedisCluster.get(key);
+	}
+
+	@Override
+	public byte[] get(final byte[] key) {
+		return jedisCluster.get(key);
+	}
+
+	@Override
+	public Boolean getbit(final String key, final long offset) {
+		return jedisCluster.getbit(key, offset);
+	}
+
+	@Override
+	public Boolean getbit(final byte[] key, final long offset) {
+		return jedisCluster.getbit(key, offset);
+	}
+
+	@Override
+	public Client getClient() {
+		throw new RuntimeException(EXCEPTION_MSG);
+	}
+
+	@Override
+	public Long getDB() {
+		return jedisCluster.getDB();
+	}
+
+	@Override
+	public String getrange(final String key, final long startOffset, final long endOffset) {
+		return jedisCluster.getrange(key, startOffset, endOffset);
+	}
+
+	@Override
+	public byte[] getrange(final byte[] key, final long startOffset, final long endOffset) {
+		return jedisCluster.getrange(key, startOffset, endOffset);
+	}
+
+	@Override
+	public String getSet(final String key, final String value) {
+		return jedisCluster.getSet(key, value);
+	}
+
+	@Override
+	public byte[] getSet(final byte[] key, final byte[] value) {
+		return jedisCluster.getSet(key, value);
+	}
+
+	protected static String[] getParams(List<String> keys, List<String> args) {
+		return getParams(keys, args);
+	}
+
+	protected static byte[][] getParamsWithBinary(List<byte[]> keys, List<byte[]> args) {
+		return getParamsWithBinary(keys, args);
+	}
+
+	@Override
+	public Long hdel(final String key, final String... fields) {
+		return jedisCluster.hdel(key, fields);
+	}
+
+	@Override
+	public Long hdel(final byte[] key, final byte[]... fields) {
+		return jedisCluster.hdel(key, fields);
+	}
+
+	@Override
+	public Boolean hexists(final String key, final String field) {
+		return jedisCluster.hexists(key, field);
+	}
+
+	@Override
+	public Boolean hexists(final byte[] key, final byte[] field) {
+		return jedisCluster.hexists(key, field);
+	}
+
+	@Override
+	public String hget(final String key, final String field) {
+		return jedisCluster.hget(key, field);
+	}
+
+	@Override
+	public byte[] hget(final byte[] key, final byte[] field) {
+		return jedisCluster.hget(key, field);
+	}
+
+	@Override
+	public Map<String, String> hgetAll(final String key) {
+		return jedisCluster.hgetAll(key);
+	}
+
+	@Override
+	public Map<byte[], byte[]> hgetAll(final byte[] key) {
+		return jedisCluster.hgetAll(key);
+	}
+
+	@Override
+	public Long hincrBy(final String key, final String field, final long value) {
+		return jedisCluster.hincrBy(key, field, value);
+	}
+
+	@Override
+	public Long hincrBy(final byte[] key, final byte[] field, final long value) {
+		return jedisCluster.hincrBy(key, field, value);
+	}
+
+	@Override
+	public Double hincrByFloat(final String key, final String field, final double value) {
+		return jedisCluster.hincrByFloat(key, field, value);
+	}
+
+	@Override
+	public Double hincrByFloat(final byte[] key, final byte[] field, final double value) {
+		return jedisCluster.hincrByFloat(key, field, value);
+	}
+
+	@Override
+	public Set<String> hkeys(final String key) {
+		return jedisCluster.hkeys(key);
+	}
+
+	@Override
+	public Set<byte[]> hkeys(final byte[] key) {
+		return jedisCluster.hkeys(key);
+	}
+
+	@Override
+	public Long hlen(final String key) {
+		return jedisCluster.hlen(key);
+	}
+
+	@Override
+	public Long hlen(final byte[] key) {
+		return jedisCluster.hlen(key);
+	}
+
+	@Override
+	public List<String> hmget(final String key, final String... fields) {
+		return jedisCluster.hmget(key, fields);
+	}
+
+	@Override
+	public List<byte[]> hmget(final byte[] key, final byte[]... fields) {
+		return jedisCluster.hmget(key, fields);
+	}
+
+	@Override
+	public String hmset(final String key, final Map<String, String> hash) {
+		return jedisCluster.hmset(key, hash);
+	}
+
+	@Override
+	public String hmset(final byte[] key, final Map<byte[], byte[]> hash) {
+		return jedisCluster.hmset(key, hash);
+	}
+
+	@Override
+	public ScanResult<Map.Entry<byte[], byte[]>> hscan(final byte[] key, final byte[] cursor) {
+		return jedisCluster.hscan(key, cursor);
+	}
+
+	@Override
+	public ScanResult<Map.Entry<String, String>> hscan(final String key, final String cursor) {
+		return jedisCluster.hscan(key, cursor);
+	}
+
+	@Override
+	public ScanResult<Map.Entry<String, String>> hscan(final String key, final int cursor) {
+		return jedisCluster.hscan(key, cursor);
+	}
+
+	@Override
+	public ScanResult<Map.Entry<byte[], byte[]>> hscan(final byte[] key, final byte[] cursor,
+			final ScanParams params) {
+		return jedisCluster.hscan(key, cursor, params);
+	}
+
+	@Override
+	public ScanResult<Map.Entry<String, String>> hscan(final String key, final String cursor,
+			final ScanParams params) {
+		return jedisCluster.hscan(key, cursor, params);
+	}
+
+	@Override
+	public ScanResult<Map.Entry<String, String>> hscan(final String key, final int cursor,
+			final ScanParams params) {
 //		Jedis jedis;
-//		jedis
-		return jedisCluster.eval(script, keys, args);
+//		jedis;
+		throw new RuntimeException(EXCEPTION_MSG);
 	}
 }
