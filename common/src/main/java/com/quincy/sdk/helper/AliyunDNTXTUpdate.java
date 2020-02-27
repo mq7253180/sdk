b5d547cc30a082ab24142b4b73f7be292a6c2711
Qiang Ma <mq7253180@126.com>
2020-02-27 21:53:34 +0800

    xxx

--- common/src/main/java/com/quincy/sdk/helper/AliyunDNTXTUpdate.java
+++ common/src/main/java/com/quincy/sdk/helper/AliyunDNTXTUpdate.java
@@ -38,43 +38,24 @@ public class AliyunDNTXTUpdate {
 		Calendar c = Calendar.getInstance();
 		int zoneOffset = c.get(Calendar.ZONE_OFFSET);
 		c.add(Calendar.HOUR, -(zoneOffset/1000/3600));
-		String _timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(c.getTime());
-		String timestamp = _timestamp.replaceAll("\\s+", "T")+"Z";
+		String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(c.getTime());
+		timestamp = timestamp.replaceAll("\\s+", "T")+"Z";
 		timestamp = URLEncoder.encode(timestamp, CHARSET_UTF8);
-		StringBuilder params = new StringBuilder(1000);
-		params.append("AccessKeyId=");
-		params.append(id);
-		params.append("&Action=");
-		params.append(action);
-		if("DescribeDomainRecords".equals(action)) {
-			params.append("&DomainName=");
-			params.append(domain);
-		}
+		StringBuilder params = new StringBuilder(1000).append("AccessKeyId=").append(id).append("&Action=").append(action);
+		if("DescribeDomainRecords".equals(action))
+			params.append("&DomainName=").append(domain);
 		params.append("&Format=JSON");
-		if(ACTION_UPDATE.equals(action)) {
-			String recordId = args[3];
-			params.append("&RR=%2A&RecordId=");
-			params.append(recordId);
-		}
-		params.append("&SignatureMethod=HMAC-SHA1");
-		params.append("&SignatureNonce=");
-		params.append(signatureNonce);
-		params.append("&SignatureVersion=1.0");
-		params.append("&Timestamp=");
-		params.append(timestamp);
-		if(ACTION_UPDATE.equals(action)) {
-			String value = args[4];
-			params.append("&Type=TXT");
-			params.append("&Value=");
-			params.append(value);
-		}
+		if(ACTION_UPDATE.equals(action))
+			params.append("&RR=%2A&RecordId=").append(args[3]);
+		params.append("&SignatureMethod=HMAC-SHA1&SignatureNonce=").append(signatureNonce).append("&SignatureVersion=1.0&Timestamp=").append(timestamp);
+		if(ACTION_UPDATE.equals(action))
+			params.append("&Type=TXT&Value=").append(args[4]);
 		params.append("&Version=2015-01-09");
 		String stringToSign = "GET&%2F&"+URLEncoder.encode(params.toString(), CHARSET_UTF8);
 		String signature = SecurityHelper.encrypt(HmacAlgorithms.HMAC_SHA_1.getName(), CHARSET_UTF8, secret+"&", stringToSign);
 		String urlEncodedSignature = URLEncoder.encode(signature, CHARSET_UTF8);
 		System.out.println(stringToSign+"\r\n"+signature+"\r\n"+urlEncodedSignature);
-		params.append("&Signature=");
-		params.append(urlEncodedSignature);
+		params.append("&Signature=").append(urlEncodedSignature);
 		String url = HTTP_PREFIX+params.toString();
 		System.out.println(url);
 //		System.out.println(URLDecoder.decode("%2A", CHARSET_UTF8)+"---"+URLEncoder.encode("*", CHARSET_UTF8));
