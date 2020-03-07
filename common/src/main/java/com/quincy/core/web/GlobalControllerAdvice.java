b565cadb0a0fd1449e932d8603073c1b2d501b56
Qiang Ma <mq7253180@126.com>
2020-03-07 12:32:38 +0800

    xxx

--- common/src/main/java/com/quincy/core/web/GlobalControllerAdvice.java
+++ common/src/main/java/com/quincy/core/web/GlobalControllerAdvice.java
@@ -3,25 +3,14 @@ package com.quincy.core.web;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.support.RequestContext;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.quincy.sdk.Result;
-import com.quincy.sdk.helper.HttpClientHelper;
-
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 //@ControllerAdvice
 public class GlobalControllerAdvice {
-    @ResponseBody
 //    @ExceptionHandler(value = Exception.class)
-    public Result handleGlobleException(HttpServletRequest request, Exception e) throws IOException {
-    	log.error("EXCEPTION_MSG: "+HttpClientHelper.getRequestURIOrURL(request, "URL"), e);
-    	RequestContext requestContext = new RequestContext(request);
-    	Result result = Result.newException();
-    	result.msg(requestContext.getMessage(Result.I18N_KEY_EXCEPTION)).data(e.getClass().getName()+": "+e.getMessage());
-    	return result;
+    public ModelAndView handleGlobleException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) throws IOException {
+    	return new GlobalHandlerExceptionResolver().resolveException(request, response, handler, e);
     }
 }
