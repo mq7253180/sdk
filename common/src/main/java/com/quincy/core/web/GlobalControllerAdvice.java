51c170c73e2bf6af3f4e811c2518efe67d1184a0
Qiang Ma <mq7253180@126.com>
2020-03-08 00:09:10 +0800

    xxx

--- common/src/main/java/com/quincy/core/web/GlobalControllerAdvice.java
+++ common/src/main/java/com/quincy/core/web/GlobalControllerAdvice.java
@@ -1,7 +1,5 @@
 package com.quincy.core.web;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 //@ControllerAdvice
 public class GlobalControllerAdvice {
 //    @ExceptionHandler(value = Exception.class)
-    public ModelAndView handleGlobleException(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
+    public ModelAndView handleGlobleException(HttpServletRequest request, HttpServletResponse response, Exception e) {
     	return new GlobalHandlerExceptionResolver().resolveException(request, response, null, e);
     }
 }
