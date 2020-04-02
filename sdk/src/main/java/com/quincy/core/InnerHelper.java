517cc5b06a1f5a865fcf48ba260310add1ab9fd5
Qiang Ma <mq7253180@126.com>
2020-04-02 22:00:07 +0800

    xxx

--- sdk/src/main/java/com/quincy/core/InnerHelper.java
+++ sdk/src/main/java/com/quincy/core/InnerHelper.java
@@ -13,21 +13,19 @@ import com.quincy.sdk.helper.CommonHelper;
 import com.quincy.sdk.helper.HttpClientHelper;
 
 public class InnerHelper {
-	public static void outputOrRedirect(HttpServletRequest request, HttpServletResponse response, Object handler, int status, String msgI18NKey, String _redirectTo, boolean appendBackTo) throws IOException, ServletException {
+	public static void outputOrRedirect(HttpServletRequest request, HttpServletResponse response, Object handler, int status, String msgI18NKey, String redirectTo, boolean appendBackTo) throws IOException, ServletException {
 		String clientType = CommonHelper.clientType(request, handler);
 		if(InnerConstants.CLIENT_TYPE_J.equals(clientType)) {
 			RequestContext requestContext = new RequestContext(request);
 			String outputContent = "{\"status\":"+status+", \"msg\":\""+requestContext.getMessage(msgI18NKey)+"\"}";
 			HttpClientHelper.outputJson(response, outputContent);
-		} else {
-			StringBuilder redirectTo = new StringBuilder(250).append(_redirectTo);
-			if(appendBackTo)
-				redirectTo
-					.append(_redirectTo.indexOf("?")>=0?"&":"?")
+		} else
+			request.getRequestDispatcher(appendBackTo?new StringBuilder(250)
+					.append(redirectTo)
+					.append(redirectTo.indexOf("?")>=0?"&":"?")
 					.append(InnerConstants.PARAM_REDIRECT_TO)
 					.append("=")
-					.append(URLEncoder.encode(request.getRequestURI()+(request.getQueryString()==null?"":("?"+request.getQueryString())), "UTF-8"));
-			request.getRequestDispatcher(redirectTo.toString()).forward(request, response);
-		}
+					.append(URLEncoder.encode(request.getRequestURI()+(request.getQueryString()==null?"":("?"+request.getQueryString())), "UTF-8"))
+					.toString():redirectTo).forward(request, response);
 	}
 }
