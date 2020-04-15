e6fee4375ba72a2664ace200108e01ecdbd4265e
Qiang Ma <mq7253180@126.com>
2020-04-15 16:24:30 +0800

    xxx

---
+++ auth-client/src/main/java/com/quincy/auth/AuthClientConfiguration.java
@@ -0,0 +1,8 @@
+package com.quincy.auth;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthClientConfiguration {
+	
+}
---
+++ auth-resource/src/main/java/com/quincy/auth/interceptor/OAuth2ResourceInterceptor.java
@@ -0,0 +1,28 @@
+package com.quincy.auth.interceptor;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.quincy.auth.annotation.LoginRequired;
+
+public class OAuth2ResourceInterceptor extends HandlerInterceptorAdapter {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+		if(handler instanceof HandlerMethod) {
+			boolean pass = false;
+			HandlerMethod method = (HandlerMethod)handler;
+			boolean loginRequired = method.getMethod().getDeclaredAnnotation(LoginRequired.class)!=null;
+			if(loginRequired) {
+				
+			}
+			return pass;
+		} else
+			return true;
+	}
+}
