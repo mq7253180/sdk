eef7a6a08378159ad3f5a454ce4467c77b95737d
Qiang Ma <mq7253180@126.com>
2020-06-24 12:42:34 +0800

    xxx

--- auth-common/src/main/java/com/quincy/auth/o/DSession.java
+++ auth-common/src/main/java/com/quincy/auth/o/DSession.java
@@ -1,7 +1,9 @@
 package com.quincy.auth.o;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 import lombok.Data;
 
@@ -12,4 +14,6 @@ public class DSession implements Serializable {
 	private List<String> roles;
 	private List<String> permissions;
 	private List<Menu> menus;
+	private Map<String, BigDecimal> currencyAccounts;
+	private Map<String, Serializable> attributes;
 }
