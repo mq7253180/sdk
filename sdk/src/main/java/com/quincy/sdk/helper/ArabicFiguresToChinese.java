da28006d79745d2c908efcc9198282f43ccf930a
mq7253180 <mq7253180@126.com>
2024-04-11 18:35:49 +0800

    xxx

--- sdk/src/main/java/com/quincy/sdk/helper/ArabicFiguresToChinese.java
+++ sdk/src/main/java/com/quincy/sdk/helper/ArabicFiguresToChinese.java
@@ -6,6 +6,7 @@ public class ArabicFiguresToChinese {
 	private final static int[] DIVISORS = {1000, 100, 10, 1};
 	private final static String[] UNITS = {"仟", "佰", "拾", ""};
 	private final static String[] CHN_DIGITS = {"", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
+	private final static int DIVISORS_LENGTH_1 = DIVISORS.length-1;
 
 	public static String transfer(int from) {
 		StringBuilder sb = new StringBuilder(20);
@@ -16,18 +17,17 @@ public class ArabicFiguresToChinese {
 			if(p<bigDivisor)
 				continue;
 			int deductor = 0;
-			int x = p/bigDivisor;
-			p = p%bigDivisor;
+			int x = p/bigDivisor;//取高位
+			p = p%bigDivisor;//取低位
 			if(x<1000&&sb.length()>0)
 				sb.append("零");
 			for(int j=0;j<DIVISORS.length;j++) {
 				int divisor = DIVISORS[j];
 				if(x<divisor)
 					continue;
-				int t = x-deductor;
-				int r = t/DIVISORS[j];
+				int r = (x-deductor)/DIVISORS[j];
 				if(r==0) {
-					if(j<DIVISORS.length-1&&previousIsNot0) {//不是个位&&前一位不是零
+					if(j<DIVISORS_LENGTH_1&&previousIsNot0) {//不是个位&&前一位不是零
 						sb.append("零");
 						previousIsNot0 = false;
 					}
