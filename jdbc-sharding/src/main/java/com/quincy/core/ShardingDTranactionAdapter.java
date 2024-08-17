98ebe3f109903f6edef4a927e64f981be8e8f180
mq7253180 <mq7253180@126.com>
2024-08-18 04:47:57 +0800

    xxx

--- jdbc-sharding/src/main/java/com/quincy/core/ShardingDTranactionAdapter.java
+++ jdbc-sharding/src/main/java/com/quincy/core/ShardingDTranactionAdapter.java
@@ -9,19 +9,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.quincy.core.aspect.DistributedTransactionAop;
 import com.quincy.sdk.ShardingDTranactionOptRegistry;
+import com.quincy.sdk.annotation.sharding.ShardingKey;
 
 @Component
 public class ShardingDTranactionAdapter extends DistributedTransactionAop implements ShardingDTranactionOptRegistry {
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
 	@Override
-	public void resume(Integer shardingKey)
+	public void resume(@ShardingKey Integer shardingKey)
 			throws ClassNotFoundException, NoSuchMethodException, IOException, InterruptedException {
 		this.resume();
 	}
 
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
 	@Override
-	public void resume(Integer shardingKey, String flagForCronJob)
+	public void resume(@ShardingKey Integer shardingKey, String flagForCronJob)
 			throws ClassNotFoundException, NoSuchMethodException, IOException, InterruptedException {
 		this.resume(flagForCronJob);
 	}
