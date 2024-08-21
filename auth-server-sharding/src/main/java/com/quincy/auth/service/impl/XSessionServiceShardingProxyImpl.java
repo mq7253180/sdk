8f0e11ca2c4c373453dee655d5c7183359d96074
mq7253180 <mq7253180@126.com>
2024-08-22 01:32:10 +0800

    xxx

--- auth-server-sharding/src/main/java/com/quincy/auth/service/impl/XSessionServiceShardingProxyImpl.java
+++ auth-server-sharding/src/main/java/com/quincy/auth/service/impl/XSessionServiceShardingProxyImpl.java
@@ -1,14 +1,11 @@
 package com.quincy.auth.service.impl;
 
-import org.springframework.stereotype.Service;
-
 import com.quincy.auth.o.User;
 import com.quincy.auth.o.XSession;
 import com.quincy.auth.service.XSessionServiceShardingProxy;
 import com.quincy.sdk.annotation.ReadOnly;
 import com.quincy.sdk.annotation.sharding.ShardingKey;
 
-@Service
 public class XSessionServiceShardingProxyImpl extends XSessionServiceImpl implements XSessionServiceShardingProxy {
 	@ReadOnly
 	@Override
