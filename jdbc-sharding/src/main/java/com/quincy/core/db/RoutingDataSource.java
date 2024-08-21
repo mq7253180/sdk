e4ab03822a248b8a39a2b9d346568260b78cb711
mq7253180 <mq7253180@126.com>
2024-08-21 18:09:33 +0800

    xxx

--- jdbc-sharding/src/main/java/com/quincy/core/db/RoutingDataSource.java
+++ jdbc-sharding/src/main/java/com/quincy/core/db/RoutingDataSource.java
@@ -2,8 +2,6 @@ package com.quincy.core.db;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
-import com.quincy.core.db.DataSourceHolder;
-
 public class RoutingDataSource extends AbstractRoutingDataSource {
 	@Override
 	protected Object determineCurrentLookupKey() {
