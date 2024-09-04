5db23a6e2259f81e4b09ce320f986ed1ee471808
mq7253180 <mq7253180@126.com>
2024-09-04 12:25:58 +0800

    xxx

--- sdk/src/main/java/com/quincy/core/RetValueDecator.java
+++ sdk/src/main/java/com/quincy/core/RetValueDecator.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
