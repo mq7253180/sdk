173b85bc13dcbf21e4ff086d8010c6157f84e1d3
mq7253180 <mq7253180@126.com>
2024-11-22 13:55:18 +0800

    xxx

--- auth-server-sharding/src/main/java/com/quincy/auth/service/UserServiceShardingProxy.java
+++ auth-server-sharding/src/main/java/com/quincy/auth/service/UserServiceShardingProxy.java
@@ -2,15 +2,14 @@ package com.quincy.auth.service;
 
 import com.quincy.auth.entity.UserEntity;
 import com.quincy.sdk.Client;
-import com.quincy.sdk.annotation.sharding.ShardingKey;
 import com.quincy.sdk.o.User;
 
 public interface UserServiceShardingProxy {
-	public UserEntity update(@ShardingKey long shardingKey, UserEntity vo);
-	public Long findUserId(@ShardingKey long shardingKey, String loginName);
-	public User find(@ShardingKey long shardingKey, Long id, Client client);
-	public void updatePassword(@ShardingKey long shardingKey, Long userId, String password);
-	public void add(@ShardingKey long shardingKey, UserEntity vo);
-	public boolean createMapping(@ShardingKey long shardingKey, String loginName, Long userId);
-	public int deleteMapping(@ShardingKey long shardingKey, String loginName);
+	public UserEntity update(long shardingKey, UserEntity vo);
+	public Long findUserId(long shardingKey, String loginName);
+	public User find(long shardingKey, Long id, Client client);
+	public void updatePassword(long shardingKey, Long userId, String password);
+	public void add(long shardingKey, UserEntity vo);
+	public boolean createMapping(long shardingKey, String loginName, Long userId);
+	public int deleteMapping(long shardingKey, String loginName);
 }
