1c967f017c0ef234e039eb10c783ceb1c5d14719
Ma Qiang <mq7253180@126.com>
2026-08-31 11:37:01 +0800

    xxx

--- jdbc-base/src/main/java/com/quincy/core/aspect/TbShardingAop.java
+++ jdbc-base/src/main/java/com/quincy/core/aspect/TbShardingAop.java
@@ -1,21 +1,24 @@
 package com.quincy.core.aspect;
 
+import javax.annotation.PostConstruct;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import com.quincy.core.jdbc.TbShardingConnection;
 
 @Order(7)
 @Aspect
-@Component
 public class TbShardingAop {
 	@Value("${spring.datasource.tdsharding.count}")
-	private int shardingCount;
+	private Integer shardingCount;
+	@Value("${spring.datasource.tdsharding.tableNames}")
+	private String[] tableNames;
 
 	@Pointcut("@annotation(org.springframework.transaction.annotation.Transactional)")
     public void transactionalPointCut() {}
@@ -35,5 +38,12 @@ public class TbShardingAop {
 	@Around("readOnlyPointCut()")
     public Object doReadOnlyAround(ProceedingJoinPoint joinPoint) throws Throwable {
 		return this.doAround(joinPoint);
+	}
+
+	@PostConstruct
+	public void init() {
+		Assert.isTrue(shardingCount!=null&&shardingCount>0, "spring.datasource.tdsharding.count must be specified when TbSharding is enabled!!!");
+		Assert.isTrue(tableNames!=null&&tableNames.length>0, "spring.datasource.tdsharding.tableNames must be specified when TbSharding is enabled!!!");
+		TbShardingConnection.setTableNames(tableNames);
 	}
 }
---
+++ jdbc-base/src/main/java/com/quincy/sdk/annotation/jdbc/EnableTbSharding.java
@@ -0,0 +1,18 @@
+package com.quincy.sdk.annotation.jdbc;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+
+import com.quincy.core.aspect.TbShardingAop;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import(TbShardingAop.class)
+public @interface EnableTbSharding {	
+}
